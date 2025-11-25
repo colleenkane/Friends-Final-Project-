928d9a3206a6969e8283e2931ac4486be1a0793e
colleenkane <kanec2@tcnj.edu>
2025-11-24 19:54:06 -0500

    Kanec2-final_project.R

--- Kanec2-final_project.R
+++ Kanec2-final_project.R
@@ -1,21 +1,42 @@
-# set working directory 
+# Set Working Directory 
 setwd("/courses/STA145/kanec2")
 
-### Load packages 
-# Note: Run base.R if these commands return an error 
+# Load packages 
 library(readr)
 data <- read_csv("data.csv")
 
-# quant var
+# Descriptive Statistics Table 
 summary(data$ratings_episodes)
-summary(data$male_characters)
+summary(data$episode_awards)
 
-# box plot 
-boxplot(data$ratings_episodes)
+# Correlation Test 
+cor.test(data$ratings_episodes, data$episode_awards)
+
+# Linear Regression 
+model <- lm(episode_awards ~ ratings_episodes, data = data)
+summary(model)
+
+# Regression 
+lm(episode_awards ~ ratings_episodes, data = data)
+
+# Creation of Residual 
+model <- lm(episode_awards ~ ratings_episodes, data = data)
+plot(fitted(model),
+     residuals(model),
+     xlab = "Fitted Values",
+     ylab = "Residuals",
+     main = "Residuals vs Fitted Values")
+abline(h = 0)
+plot(model$model$ratings_episodes,
+     residuals(model),
+     xlab = "Ratings",
+     ylab = "Residuals",
+     main = "Residuals vs Ratings")
+abline(h = 0)
 
 # Creation of Scatter Plot 
 plot(x = data$ratings_episodes,
      y = data$episode_awards,
      xlab = "Episode Ratings",
      ylab = "Episode Awards",
-     main = "Episode Ratings vs Episode Awards")
+     main = "Episode Awards vs Episode Ratings")
